fa6c3c9eb5fd687251a15daf709726a1a9664e57
David Fernández Puentes <david.fernandez@and-proy-02.andaltec.local>
2015-11-27 09:32:48 +0100

    Carga del fichero excel con POI

--- testExcel/src/testexcel/TestExcel.java
+++ testExcel/src/testexcel/TestExcel.java
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testexcel;
 
+/**
+ * Inclusión de las librerías
+ */
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -24,19 +22,21 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 
-
 /**
  * Aplicación para la lectura de ficheros excel desde java
+ *
  * @author david.fernandez
  */
 public class TestExcel {
-    
+
     /**
-     * Función que lee el número de hojas de un excel pasado cómo parámetros 
-     * y muestra por pantalla el número de hojas y el nombre de estas
+     * Función que lee el número de hojas de un excel pasado cómo parámetros y
+     * muestra por pantalla el número de hojas y el nombre de estas
+     *
      * @param archivo , con la ruta del archivo Excel
-     **/
-    public static void leerExcelJXL(File archivo){
+     *
+     */
+    public static void leerExcelJXL(File archivo) {
         try {
             Workbook libro = Workbook.getWorkbook(archivo);
             //Mostramos información de libro
@@ -44,69 +44,78 @@ public class TestExcel {
             String[] hojas = libro.getSheetNames();
             System.out.println("Número de hojas: " + numHojas);
             for (String hoja : hojas) {
-                System.out.println("Hoja: " + hoja); 
+                System.out.println("Hoja: " + hoja);
             }
             //Ahora vamos a crear un nuevo fichero xls y le vamos a copiar 
             //algunos elementos del original. 
-            
+
             ///Creamos el libro con posibilidad de escritura
             WritableWorkbook salida = Workbook.createWorkbook(new File("E:\\salida.xls"));
-            WritableSheet ws = salida.createSheet("salida", 0); 
+            WritableSheet ws = salida.createSheet("salida", 0);
             ws.setName("output2");
-        
+
         } catch (IOException | BiffException ex) {
             Logger.getLogger(TestExcel.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public static void leerExcelPOI(String rutaPoi) throws IOException{
+
+    public static void leerExcelPOI(String rutaPoi) throws IOException {
         List sheetData = new ArrayList();
         try (FileInputStream fis = new FileInputStream(rutaPoi)) {
-			//
-			// Create an excel workbook from the file system.
-			//
-			HSSFWorkbook workbook = new HSSFWorkbook(fis);
-			//
-			// Get the first sheet on the workbook.
-			//
-			HSSFSheet sheet = workbook.getSheetAt(0);
-			//
-			// When we have a sheet object in hand we can iterator on
-			// each sheet's rows and on each row's cells. We store the
-			// data read on an ArrayList so that we can printed the
-			// content of the excel to the console.
-			//
-			Iterator rows = sheet.rowIterator();
-			while (rows.hasNext()) {
-				HSSFRow row = (HSSFRow) rows.next();
-				
-				Iterator cells = row.cellIterator();
-				List data = new ArrayList();
-				while (cells.hasNext()) {
-					HSSFCell cell = (HSSFCell) cells.next();
-				//	System.out.println("Añadiendo Celda: " + cell.toString());
-					data.add(cell);
-				}
-				sheetData.add(data);
-			}
-		} catch (IOException e) {
-		}
-		showExelData(sheetData);
-	}
+            /**
+             * Crea un objeto excel desde el sistema de ficheros
+             */
+            HSSFWorkbook workbook = new HSSFWorkbook(fis);
+            /**
+             * Obtiene la primera hoja del libro
+             */
+            HSSFSheet sheet = workbook.getSheetAt(0);
+            /**
+             * Cuando hay un objeto en la hoja, lo manejamos con un
+             * iterador para la hoja con rows y para cada row con sus
+             * Cells asociadas. 
+             * Almacenamos los datos en un ArrayList que podemos manejar
+             */
+            Iterator rows = sheet.rowIterator();
+            while (rows.hasNext()) {
+                HSSFRow row = (HSSFRow) rows.next();
 
- private static void showExelData(List sheetData) {
-        //
-        // Iterates the data and print it out to the console.
-        //
+                Iterator cells = row.cellIterator();
+                List data = new ArrayList();
+                while (cells.hasNext()) {
+                    HSSFCell cell = (HSSFCell) cells.next();
+                    System.out.println("Añadiendo Celda: " + cell.toString());
+                    data.add(cell);
+                }
+                sheetData.add(data);
+            }
+        } catch (IOException e) {
+        }
+        showExelData(sheetData);
+    }
+    
+    /**
+     * Función que muestra una colección de valores almacenados de
+     * una celda.
+     * @param   sheetData , lista de datos de la hoja
+     */
+    private static void showExelData(List sheetData) {
+        /**
+         * Itera y muestra por consola los datos
+         */
         for (Object sheetData1 : sheetData) {
             List list = (List) sheetData1;
             for (int j = 0; j < list.size(); j++) {
                 Cell cell = (Cell) list.get(j);
-                if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+                //Tratamiento en caso de ser de tipo numérico
+                if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) 
+                {
                     System.out.print(cell.getNumericCellValue());
-                } else if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+                } else if (cell.getCellType() == Cell.CELL_TYPE_STRING) 
+                { //Tratamiento en caso de ser String
                     System.out.print(cell.getRichStringCellValue());
-                } else if (cell.getCellType() == Cell.CELL_TYPE_BOOLEAN) {
+                } else if (cell.getCellType() == Cell.CELL_TYPE_BOOLEAN)
+                { //Tratamiento en caso de ser Booleano
                     System.out.print(cell.getBooleanCellValue());
                 }
                 if (j < list.size() - 1) {
@@ -115,20 +124,23 @@ public class TestExcel {
             }
             System.out.println("");
         }
-}   
+    }
+
     /**
+     * Función principal del programa
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        
-        String ruta="E:\\jtest.xls";
+
+        String ruta = "E:\\jtest.xls";
         File fich = new File(ruta);
         leerExcelJXL(fich);
-        
+
         String rutaPoi = "test.xlsx";
         leerExcelPOI(rutaPoi);
-        
+
     }
-    
+
 }
