e8b31757bb87382bddfb98856d0bc96ef8c7caf3
David Fernández Puentes <david.fernandez@and-proy-02.andaltec.local>
2015-11-27 09:33:47 +0100

    Carga del fichero excel con POI

--- testExcel/src/testexcel/TestExcel.java
+++ testExcel/src/testexcel/TestExcel.java
@@ -5,7 +5,6 @@ package testexcel;
  */
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
