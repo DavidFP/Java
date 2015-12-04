e7c716801bd572d2b433b80646b871ac947c5472
David Fernández Puentes <David.Fernandez@and-proy-02.andaltec.local>
2015-12-04 09:22:17 +0100

    Primer commit

---
+++ src/model/Main.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ *
+ * @author David.Fernandez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
